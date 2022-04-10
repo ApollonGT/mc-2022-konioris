a817cdba5a91db31210c580fe858f8955afb8a58
Aggelos Konioris <aggelos_96@windowslive.com>
2022-04-10 13:57:08 +0300

    Update app_categories.py

--- pre-processing steps/app_categories.py
+++ pre-processing steps/app_categories.py
@@ -1,17 +1,13 @@
-#Firstly, we run the necessary code from previous file
+#Firstly, we import necessary libraries along with the df from a previous module
 
-import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-df = pd.read_csv('Google-Playstore.csv')
-df['Category'] = df['Category'].str.replace('Music & Audio', 'Music')
-df['Category'] = df['Category'].str.replace('Educational', 'Education')
+from data_cleaning import df
 
 # Creating a table with the top 10 categories based on the number of apps
 
-df.insert(24, 'Number of Apps', 1)
-group = df.groupby('Category').sum()[['Number of Apps']].reset_index()
+df.insert(17, 'Number of Apps', 1)
+group = df.groupby('category').sum()[['Number of Apps']].reset_index()
 group1 = group.sort_values(by = 'Number of Apps', ascending = False)
 top10 = group1.head(10)
 print(top10)
@@ -19,8 +15,7 @@ print(top10)
 # We can also see this table, graphically!
 
 plt.figure(figsize = (18, 9))
-sns.barplot(x = 'Category', y = 'Number of Apps', data = top10)
+sns.barplot(x = 'category', y = 'Number of Apps', data = top10)
 plt.title('Top 10 Categories based on the Number of Apps', fontsize = 15)
 plt.xlabel('Category', fontsize = 13)
 plt.ylabel('Number of Apps', fontsize = 13);
-
